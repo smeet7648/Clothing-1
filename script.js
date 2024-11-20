20b92f39545a68dbfa8f4a0187d1ea137c663fc2
smeet7648 <167986379+smeet7648@users.noreply.github.com>
2024-11-20 11:49:42 +0530

    all done

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.getElementById("imghide").addEventListener("click", function () {
     document.getElementById("imgshow").style.display = "none";
     document.getElementById("imghide").style.display = "block";
   });
+
+  let currentIndex = 0;
+  const slides = document.querySelectorAll('.f4 div img');
+  
+  function showSlide(index) {
+      slides.forEach((slide, i) => {
+          slide.classList.remove('active');
+          if (i === index) {
+              slide.classList.add('active'); 
+          }
+      });
+  }
+  
+  // Function to change slides every few seconds
+  setInterval(() => {
+      currentIndex = (currentIndex + 1) % slides.length; 
+      showSlide(currentIndex);
+  }, 3000); 
